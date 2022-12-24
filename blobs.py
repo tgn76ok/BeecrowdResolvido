9744afe77e2f5811e0f8c2baf934a26108233929
Tgn76ok <tgermanon@gmail.com>
2022-12-24 00:50:00 -0300

    estamos de volta

---
+++ blobs.py
@@ -0,0 +1,14 @@
+N = int(input(''))
+cont = 0 
+if 1<= N<= 1000:
+    for c in range(N):
+        C= float(input())
+        while True:
+            C/=2
+            cont+=1
+            if C<=1:
+                print(f"{cont} dias")
+                cont=0
+                break
+                
+else: exit()
